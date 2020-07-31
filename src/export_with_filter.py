1ea760743f2c16c80a1c4c9e05349befe03c3ab8
Anna <anna@tidepool.org>
2020-07-31 12:47:47 -0700

    Add filtering export mechanism for data

---
+++ src/export_with_filter.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import utils
+from pathlib import Path
+
+base_path = Path(__file__).parent
+data_path = (base_path / "../data/PHI-issue-reports-with-surrounding-2week-data-summary-stats-2020-07-28.csv").resolve()
+df = pd.read_csv(data_path)
+
+df = df[
+    (df.percent_70_180 >= 80)
+]
+
+print(df.count)
+export_path = (base_path / "../results/PHI-filtered-subjects-80-100.csv").resolve()
+df.to_csv(export_path)
