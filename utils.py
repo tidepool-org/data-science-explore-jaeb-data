936ce9f2e26934ca1897c8564b8ae3320fba0174
Anna <anna@tidepool.org>
2020-08-04 08:30:32 -0700

    Graphing utils

---
+++ utils.py
@@ -0,0 +1,36 @@
+import matplotlib.pyplot as plt
+
+def three_dimension_plot(x, y, z, labels=["", "", ""], title=""):
+    """
+    Function to plot a 3D graph of data, with optional labels & a title
+    """
+    assert len(labels) == 3
+
+    fig = plt.figure(1, figsize=(7, 7))
+    ax = Axes3D(fig, rect=[0, 0, 0.95, 1], elev=48, azim=134)
+    ax.scatter(x, y, z, edgecolor="k", s=50)
+    ax.set_xlabel(labels[0])
+    ax.set_ylabel(labels[1])
+    ax.set_zlabel(labels[2])
+
+    plt.title(title, fontsize=30)
+    plt.show()
+
+
+def two_dimension_plot(x, y, labels=["", ""], title="", ylim=None):
+    """
+    Function to plot a 2D graph of data, with optional labels & a title
+    """
+    assert len(labels) == 2
+
+    fig = plt.figure(1, figsize=(7, 7))
+    plt.scatter(x, y, edgecolor="k", s=50)
+    plt.xlabel(labels[0])
+    plt.ylabel(labels[1])
+
+    axes = plt.gca()
+    if ylim:
+        axes.set_ylim(ylim)
+
+    plt.title(title, fontsize=30)
+    plt.show()
