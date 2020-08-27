6454bea297772075ab6f85e96f47ef6f0124d790
Anna <anna@tidepool.org>
2020-08-27 07:28:16 -0700

    Update box plots

--- src/utils.py
+++ src/utils.py
@@ -47,45 +47,53 @@ def two_dimension_plot(x, y, labels=["", ""], title="", ylim=None):
     plt.show()
 
 
-def box_plot(y, x_tick_labels=None, axis_labels=["", ""], title=""):
+def box_plot(
+    data_to_plot,
+    group_labels=None,
+    data_axis_labels=["", ""],
+    title="",
+    should_be_vertical=True,
+):
     """ Plot a box plot """
     fig = plt.figure(1, figsize=(9, 6))
     ax = fig.add_subplot(111)
-    ax.boxplot(y)
+    ax.boxplot(data_to_plot, vert=should_be_vertical)
     ax.get_xaxis().tick_bottom()
     ax.get_yaxis().tick_left()
 
-    if x_tick_labels:
-        assert len(x_tick_labels) == len(y)
-        ax.set_xticklabels(x_tick_labels)
+    if group_labels:
+        assert len(group_labels) == len(data_to_plot)
+        ax.set_xticklabels(group_labels)
 
-    plt.xlabel(axis_labels[0])
-    plt.ylabel(axis_labels[1])
+    plt.xlabel(data_axis_labels[0])
+    plt.ylabel(data_axis_labels[1])
     plt.title(title, fontsize=30)
 
     plt.show()
 
-def generate_boxplot_data(df, y_data_key, range, x_data_key=None):
+
+def generate_boxplot_data(df, y_data_key, range, x_data_key=None, interval=1):
     """
     df - dataframe with Jaeb data
     y_data_key - string of column that should have the boxplot generated
     range - range iterable to use to determine windows
     x_data_key - string of column that have boxplots generated over specific 
     ranges of this column
+    interval - interval to make windows
     """
-    '''
+    """
     If we only want to plot 1 datatype on the box plot 
     (like just BMI box plot instead of a series of BMI 
     plots based on particular ages), then x_data_key should
     be same as y_data_key
-    '''
+    """
     if not x_data_key:
         x_data_key = y_data_key
 
     boxplot_data = []
     for val in range:
-        filtered = df[(df[x_data_key] >= val) & (df[x_data_key] < val + 1)]
+        filtered = df[(df[x_data_key] >= val) & (df[x_data_key] < val + interval)]
         boxplot_data.append(filtered[y_data_key].tolist())
-    
+
     ticks = [str(val) for val in range]
     return (boxplot_data, ticks)
