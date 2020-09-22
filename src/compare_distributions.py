4d6165ca52e56cd6f2dbf13971ac1aa2e544e7d5
Anna <anna@tidepool.org>
2020-09-22 13:43:37 -0700

    First pass at finding normal distrib

--- src/compare_distributions.py
+++ src/compare_distributions.py
@@ -4,21 +4,24 @@ import numpy as np
 import utils
 
 carb_key = "total_daily_carb_avg"  # Total daily CHO
-t1d_isf_key = "InsulinSensitivity"
 jaeb_isf_key = "isf"
-
-t1d_exchange_path = utils.find_full_path("t1d_exchange", ".csv")
-t1d_exchange = pd.read_csv(t1d_exchange_path)
+icr_key = "carb_ratio"
 
 jaeb_path = utils.find_full_path("PHI-unique-settings-with-3hr-hysteresis-from-all-data-five-minute-8hr-outcomes-2020-08-19-23-v0-1-0-ed", ".csv")
 jaeb = pd.read_csv(jaeb_path)
 
 isf = jaeb[jaeb_isf_key].replace(0, np.nan).dropna()
 log_isf = np.log(isf)
-statistic, p_value = stats.kstest(log_isf, 'norm')
+# Normalize mean to 0
+normed_log_isf = (log_isf - log_isf.mean(axis=0)) / log_isf.std(axis=0)
+statistic, p_value = stats.kstest(normed_log_isf, 'norm')
 print(statistic, p_value)
 
-k, p_2 = stats.normaltest(log_isf)
-print(k, p_2)
+icr = jaeb[icr_key].replace(0, np.nan).dropna()
+log_icr = np.log(icr)
+# Normalize mean to 0
+normed_log_icr = (log_icr - log_icr.mean(axis=0)) / log_icr.std(axis=0)
+statistic, p_value = stats.kstest(normed_log_icr, 'norm')
+print(statistic, p_value)
 
 
