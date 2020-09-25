11156027fac0de317afbea6ccaa3a0a563e9a0bf
Anna <anna@tidepool.org>
2020-09-25 11:27:50 -0700

    Update get_distributions to export a file

--- src/get_distributions.py
+++ src/get_distributions.py
@@ -3,9 +3,14 @@ import numpy as np
 import utils
 import math
 
-# data_path = utils.find_full_path("PHI-unique-settings-with-3hr-hysteresis-from-all-data-five-minute-8hr-outcomes-2020-08-19-23-v0-1-0-ed", ".csv")
-data_path = utils.find_full_path("t1d_exchange", ".csv")
+# file_name = "PHI-unique-settings-with-3hr-hysteresis-from-all-data-five-minute-8hr-outcomes-2020-08-19-23-v0-1-0-ed"
+file_name = "t1d_exchange"
+data_path = utils.find_full_path(file_name, ".csv")
 df = pd.read_csv(data_path)
+
+distribution_stats = ["mean", "std", "min", "25%", "75%", "max"]
+output_df = pd.DataFrame(columns=distribution_stats)
+analysis_name = "analyze-demographics"
 
 # Keys for working with exports
 """ Jaeb """
@@ -33,7 +38,15 @@ keys = [isf_key, icr_key, carb_key, bmi_key]
 
 """ Print out distributions for all the different columns """
 for key in keys:
-    print(df[key].describe())
-    print()
+    row = []
+    distribution = df[key].describe(include="all")
+    for stat in distribution_stats:
+        row.append(distribution.loc[stat])
+    assert len(row) == len(distribution_stats)
+    output_df.loc[key] = row
 
-relevent_data = df[keys]
+output_df.to_csv(
+    utils.get_save_path_with_file(
+        file_name, analysis_name, "distributions.csv", "data-processing"
+    )
+)
