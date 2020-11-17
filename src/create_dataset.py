2c6879f2b7b4517be20a9f0b82bd7a520b286996
Anna <anna@tidepool.org>
2020-11-17 14:24:12 -0800

    feat: add annotation with demographic info

--- src/create_dataset.py
+++ src/create_dataset.py
@@ -26,6 +26,7 @@ tir = "percent_70_180"
 percent_below_40 = "percent_below_40"
 percent_below_54 = "percent_below_54"
 percent_below_70 = "percent_below_70"
+percent_above_180 = "percent_above_180"
 percent_above_250 = "percent_above_250"
 percent_above_400 = "percent_above_400"
 percent_cgm = "percent_cgm_available"
@@ -34,16 +35,15 @@ loop_id = "loop_id"
 percent_true = "percent_true_over_outcome"
 
 # From the survey data
-bmi = "bmi_at_baseline"
-bmi_percentile = "bmi_perc_at_baseline"
-age = "age_at_baseline"
+bmi = "BMI"
+bmi_percentile = "BMIPercentile"
+age = "Age"
 
 # From our aggregation
 num_reports = "num_reports_used"
 
-aggregate_output_rows = [
+rows_without_demographic_data = [
     loop_id,
-    num_reports,
     percent_true,
     percent_cgm,
     tdd,
@@ -55,12 +55,13 @@ aggregate_output_rows = [
     percent_below_54,
     percent_below_70,
     tir,
+    percent_above_180,
     percent_above_250,
     percent_above_400,
-    age,
-    bmi,
-    bmi_percentile,
 ]
+
+aggregate_output_rows = rows_without_demographic_data.copy()
+aggregate_output_rows.extend([age, bmi, bmi_percentile, num_reports])
 
 input_directory = "phi-all-setting-schedule_dataset-basal_minutes_30-outcome_hours_3-expanded_windows_False-days_around_issue_report_7"
 analysis_name = "make_dataset"
@@ -71,38 +72,28 @@ all_patient_files = glob.glob(
 all_output_rows_df = None
 
 for file_path in all_patient_files:
-    # print("Analyzing file at {}".format(file_path))
+    print("Loading file at {}".format(file_path))
     df = pd.read_csv(file_path)
 
     # Initialize our df using the column data from our first file
     if all_output_rows_df is None:
         all_output_rows_df = pd.DataFrame(columns=df.columns)
 
-    df.dropna(
-        subset=[
-            tdd,
-            total_daily_basal,
-            total_daily_carbs,
-            isf,
-            icr,
-            tir,
-            percent_below_40,
-            percent_below_54,
-            percent_below_70,
-            percent_above_250,
-            percent_above_400,
-            percent_cgm,
-        ]
-    )
+    df.dropna(subset=rows_without_demographic_data)
 
     # Don't include data where Loop wasn't running for most of the time
-    # TODO: consult with others about this threshold once we have rest of data
-    if df[percent_true].max() < 70:
+    # TODO: consult with the others about this threshold once we have rest of data
+    if df[percent_true].max() < 75:
         continue
 
     best_rows = df[
         (df[percent_true] == df[percent_true].max()) & (df[percent_cgm] >= 90)
     ]
+
+    if len(best_rows.index) < 1:
+        print("Skipping file at {} due to no rows fitting criteria".format(file_path))
+        continue
+
     all_output_rows_df = all_output_rows_df.append(best_rows.iloc[0], ignore_index=True)
 
 short_file_name = "processed-30-min-win"
@@ -123,10 +114,11 @@ patient_dfs = all_output_rows_df.groupby(loop_id)
 all_patients_df = pd.DataFrame(columns=aggregate_output_rows)
 print("We have {} unique patient IDs".format(len(all_output_rows_df[loop_id].unique())))
 
+# Aggregate all the rows together for each patient
 for id, df in patient_dfs:
+    print("Aggregating data for patient {}".format(id))
     row = [
         id,
-        len(df.index),
         df[percent_true].mean(),
         df[percent_cgm].mean(),
         df[tdd].mean(),
@@ -138,17 +130,50 @@ for id, df in patient_dfs:
         df[percent_below_54].mean(),
         df[percent_below_70].mean(),
         df[tir].mean(),
+        df[percent_above_180].mean(),
         df[percent_above_250].mean(),
         df[percent_above_400].mean(),
-        # Set -1 as a placeholder in the demographics columns
-        -1,
-        -1,
-        -1,
+        # Set a placeholder in the demographics columns
+        None,
+        None,
+        None,
+        len(df.index),
     ]
+
+    # Check that percents make sense
+    assert (
+        99.9
+        <= df[percent_below_70].mean() + df[tir].mean() + df[percent_above_180].mean()
+        <= 100.1
+    )
+
     all_patients_df.loc[len(all_patients_df.index)] = row
 
-print(all_patients_df)
-# TODO: add survey data
+
+survey_data_file_name = "Primary-Outcome-Listings"
+survey_path = utils.find_full_path(survey_data_file_name, ".csv")
+survey_df = pd.read_csv(survey_path)
+survey_data_loop_id = "PtID"
+
+# Add survey data
+for i in range(len(all_patients_df.index)):
+    patient_id = all_patients_df.loc[i, loop_id]
+    print("Adding survey data for patient {}".format(patient_id))
+    rows = survey_df[survey_df[survey_data_loop_id] == patient_id]
+
+    if len(rows.index) < 1:
+        print("Couldn't find demographic info for patient {}".format(patient_id))
+        continue
+
+    row = rows.iloc[0]
+
+    all_patients_df.loc[i, age] = row[age]
+    if row[bmi] != ".":
+        all_patients_df.loc[i, bmi] = row[bmi]
+    if row[bmi_percentile] != ".":
+        all_patients_df.loc[i, bmi_percentile] = row[bmi_percentile]
+
+print(all_patients_df.head())
+
 export(all_output_rows_df, "all_selected_rows")
 export(all_patients_df, "aggregated_rows_per_patient")
-
